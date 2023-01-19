5217e12aa644983c2603637191470379300b3f1b
rodrigolb01 <rodrigolb01@gmail.com>
2023-01-19 11:40:05 -0300

    skdoaasAS

--- starter_kit/src/components/App.js
+++ starter_kit/src/components/App.js
@@ -3,10 +3,10 @@ import logo from '../logo.png';
 import './App.css';
 import { encode as base64_encode } from 'base-64';
 import File from '../abis/File.json';
-import SolRsaVerify from '../abis/SolRsaVerify.json';
+
+const crypto = require('crypto');
 
 require('dotenv').config();
-const web3 = require("web3");
 const ethers = require('ethers');
 const ipfsClient = require('ipfs-http-client');
 
@@ -18,10 +18,7 @@ const ipfs = ipfsClient({host: "ipfs.infura.io", port: "5001",  protocol: "https
 }})
 
 //If testnet is goerli
-const storageFileContractAddress = require('../abis/File.json').networks[5].address.toString();
-///
-const verifyContractAddress = require('../abis/SolRsaVerify.json').networks[5].address.toString();
-///
+const fileStorageContractAddress = require('../abis/File.json').networks[5].address.toString();
 
 class App extends Component {
 
@@ -30,15 +27,12 @@ class App extends Component {
     this.state = {
       account: null,
       provider: null,
-      storageFileContractInstance: null,
-      verifySignatureContractInstance: null,
-      storageFileBuffer: null,
-      signFileBuffer: null,
-      cid: null,
-      msg: "",
-      signature: "",
-      publicKeyModulus: "",
-      publicKeyExponent: ""
+      fileStorageContractInstance: null,
+      fileBuffer: null,
+      certificateBuffer: null,
+      privateKeyBuffer: null,
+      ipfsRedirectUrl: "",
+      ipfsHash: "",
     }
   }
 
@@ -66,16 +60,13 @@ class App extends Component {
       console.log("your provider");
       console.log(this.state.provider);
 
-      const deployedStorageFileContract = await new ethers.Contract(storageFileContractAddress, File.abi, this.state.provider.getSigner());
-      const deployedVerifySignatureContract = await new ethers.Contract(verifyContractAddress, SolRsaVerify.abi, this.state.provider.getSigner());
+      const deployedFileStorageContract = await new ethers.Contract(fileStorageContractAddress, File.abi, this.state.provider.getSigner());
 
       await this.setState({
-        storageFileContractInstance : deployedStorageFileContract,
-        verifySignatureContractInstance: deployedVerifySignatureContract
+        fileStorageContractInstance : deployedFileStorageContract,
       });
 
       console.log(this.state.storageFileContractInstance)
-      console.log(this.state.verifySignatureContractInstance)
     }
     else 
     {
@@ -83,54 +74,81 @@ class App extends Component {
     }
   }
 
-  //upload file to the browser and save in the state as a buffer
-  captureFileForStorage = (e) => {
+   //upload to IPFS and store hash in Ethereum
+   onSubmit = async(e) => {
     e.preventDefault();
-    const data = e.target.files[0];
-    const reader = new window.FileReader();
-    reader.readAsArrayBuffer(data);
-    reader.onloadend = () => {
-      this.setState({storageFileBuffer : Buffer(reader.result)})
-    }
-  }
 
-  captureFileForSigning = (e) => {
-    e.preventDefault();
-    const data = e.target.files[0];
-    const reader = new window.FileReader();
-    reader.readAsArrayBuffer(data);
-    reader.onloadend = () => {
-      this.setState({signFileBuffer : Buffer(reader.result)})
-    }
-  }
+    // this.sign(this.state.fileBuffer, this.state.certificateBuffer, this.state.privateKeyBuffer);
 
-  //upload to IPFS and recover it's CID (hash/address)
-  onSubmit = async(e) => {
-    e.preventDefault();
-    await ipfs.add(this.state.storageFileBuffer, async (error, result) => {
-      console.log('IPFS Result: ' + result);
-
+    await ipfs.add(this.state.fileBuffer, async (error, result) => {
       if(error)
         console.log('error! Failed to upload to IPFS: ' + error)
       if(result)
       {
-        this.setState(
-          {
-            cid: result[0].hash,
-          }
-        );
-
-        await this.storeHash();
+        await this.storeHash(result[0].hash);
         let res = await this.retrieveHash();
 
-        console.log("get: " + res);
+        this.setState(
+          {
+            ipfsHash: res,
+            ipfsRedirectUrl: `https://ipfs.stibits.com/${res}`
+          }
+        );
       }
     });
   }
 
-  storeHash = async() => {
+  sign = (file, certificate, privateKey) => {
+
+    const signatureScheme = 'RSASSA-PKCS1-v1_5';
+    const exponent = new Uint8Array([0x01, 0x00, 0x01]);
+    const encryption = {name: signatureScheme, hash: {name: "SHA-256"}};
+
+
+    var signature = window.crypto.subtle.sign( {name: signatureScheme}, certificate, file );
+
+    console.log("your signature" + signature);
+      
+    console.log("your file : " + file);
+    console.log("your certificate :" + certificate);
+  }
+
+  //upload file to the browser and save in the state as a buffer
+  captureFile = (e) => {
+    e.preventDefault();
+    const data = e.target.files[0];
+    const reader = new window.FileReader();
+    reader.readAsArrayBuffer(data);
+    reader.onloadend = () => {
+      this.setState({fileBuffer : Buffer(reader.result)})
+    }
+  }
+
+  //upload a valid x509 certificate to the browser and save in the state as a buffer
+  captureCertificate = (e) => {
+    e.preventDefault();
+    const data = e.target.files[0];
+    const reader = new window.FileReader();
+    reader.readAsArrayBuffer(data);
+    reader.onloadend = () => {
+      this.setState({certificateBuffer : Buffer(reader.result)})
+    }
+  }
+
+  //upload a valid RSA private key for signing
+  caturePrivateKey = (e) => {
+    e.preventDefault();
+    const data = e.target.files[0];
+    const reader = new window.FileReader();
+    reader.readAsArrayBuffer(data);
+    reader.onloadend = () => {
+      this.setState({privateKeyBuffer: Buffer(reader.result)})
+    }
+  }
+
+  storeHash = async(hash) => {
     try {
-      await this.state.storageFileContractInstance.set(this.state.cid);
+      await this.state.fileStorageContractInstance.set(hash);
     } catch (error) {
       window.alert(error);
     }
@@ -138,121 +156,66 @@ class App extends Component {
 
   retrieveHash = async() => {
     try {
-      const result = await this.state.storageFileContractInstance.get();
+      const result = await this.state.fileStorageContractInstance.get();
       return result;
     } catch (error) {
       window.alert(error);
     }
   }
 
-  GetCid = () => {
-    return this.state.cid;
-  }
-
-  VerifySignature = async(event) => {
-    //generate the signature:
-    //echo -n "hello world" | openssl dgst -sha256 -sign privatekey.pem -out | xxd -p | tr -d \\n
-
-    //extract the modulus and the exponent using:
-    //openssl asn1parse -inform pem -i -in publickey.crt -strparse 18
-
-    //reads content of the file
-    event.preventDefault();
-    console.log('your file: ' + this.state.signFileBuffer);
-    const bufferToString = this.state.signFileBuffer.toString();
-
-    //Error: hex data is odd-length 
-    console.log('your signature: ' + this.state.signature);
-    //Error: invalid arrayify value 
-    console.log('your pub key modulus: ' + this.state.publicKeyModulus);
-
-    console.log('your pub key exponent ' + this.state.publicKeyExponent);
-
-    // const e = "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000010001"
-    // const Msg = web3.utils.asciiToHex("hello world")
-    // const S = "079bed733b48d69bdb03076cb17d9809072a5a765460bc72072d687dba492afe951d75b814f561f253ee5cc0f3d703b6eab5b5df635b03a5437c0a5c179309812f5b5c97650361c645bc99f806054de21eb187bc0a704ed38d3d4c2871a117c19b6da7e9a3d808481c46b22652d15b899ad3792da5419e50ee38759560002388"
-    // const nn = "DF3EDDE009B96BC5B03B48BD73FE70A3AD20EAF624D0DC1BA121A45CC739893741B7CF82ACF1C91573EC8266538997C6699760148DE57E54983191ECA0176F518E547B85FE0BB7D9E150DF19EEE734CF5338219C7F8F7B13B39F5384179F62C135E544CB70BE7505751F34568E06981095AEEC4F3A887639718A3E11D48C240D"
-
-    // const e = this.state.publicKeyExponent;
-    // const Msg = web3.utils.asciiToHex("hello world");
-    // const S = this.state.signature;
-    // const nn = this.state.publicKeyModulus;
-
-    const e = "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000010001";
-    const Msg = web3.utils.asciiToHex(bufferToString);
-    //sign a text file with openssl dgst -sha256 -sign private.pem -out signed.txt msg.txt
-    var S = "鬇班䠻鯖ϛ氇綱ঘ⨇癚恔犼ⴇ絨䦺︪ᶕ롵쁜ퟳ똃뗪�季ꔃ籃尊錗脉嬯靜ͥ왡뱅Ԇ넞벇瀊퍎㶍⡌ꅱ섗涛�䠈䘜⚲텒襛펚⵹䆥傞㣮镵`蠣";
-    S = web3.utils.asciiToHex(S);
-    const nn = "B8BAE1F65760E0262B2DC9D82B4151ED3CBEB12DFE5A9C60E0E9353597B1541290CB800A32FED61A864170A9314E8250E95AB95063CC3AC0E8C17CC5FCE14B780B01B8B23A0BCC31D31E6DC008C783B65CC354A3BB7B732C3F44380CFE01A5921106126B427B1BBC9A873B0B9772104EEEA8614F7D2480D7CCEA7DE09749F32F";
-
-    const result = await this.state.verifySignatureContractInstance.pkcs1Sha256VerifyRaw(Msg, S,"0x"+e,"0x"+nn);
-    console.log('Signature is valid: ' + result);
-  }
 
   render() {
     return (
       <div>
-        <div className='picture upload'>
+        <div className='file upload'>
           <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-            <a
-              className="navbar-brand col-sm-3 col-md-2 mr-0"
-              href="http://www.dappuniversity.com/bootcamp"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              IPFS File Uploader
-            </a>
           </nav>
           <div className="container-fluid mt-5">
             <div className="row">
               <main role="main" className="col-lg-12 d-flex text-center">
                 <div className="content mr-auto ml-auto">
-                  <a
-                    href="http://www.dappuniversity.com/bootcamp"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <img src={logo} className="App-logo" alt="logo" />
-                  </a>
+                  <img src={logo} className="App-logo" alt="logo" />
                   <p>&nbsp;</p>
                   <h2>Upload a file to IPFS</h2>
                   <form onSubmit={this.onSubmit}>
-                    <input type="file" onChange={this.captureFileForStorage}/>
-                    <input type="submit"/>
+                    <div>
+                      <label>upload diploma file</label>
+                      <input type="file" onChange={this.captureFile}/>
+                    </div>
+                    <div>
+                      <label> upload your certificate</label>
+                      <input type="file" onChange={this.captureCertificate}></input>
+                    </div>
+                    <div>
+                      <label> upload your private key</label>
+                      <input type="file" onChange={this.caturePrivateKey}></input>
+                    </div>
+                    <input type="submit" title='sign' />
                     <p>&nbsp;</p>
-                    { this.state.cid ? `Your CID: ${this.GetCid()}` : "" }
-                    <p>&nbsp;</p>
-                    <img src={`https://ipfs.io/ipfs/${this.GetCid()}`} alt=""></img>
+                    <div className="results">
+                      <div>
+                        <h4>
+                          {this.state.ipfsRedirectUrl !== "" ? "Your file has been uploaded to Ipfs use the link below to access it" : ""}
+                        </h4>
+                        <a href={this.state.ipfsRedirectUrl !== "" ? this.state.ipfsRedirectUrl : ""}>
+                          {this.state.ipfsRedirectUrl !== "" ? this.state.ipfsRedirectUrl : ""}
+                        </a>
+                      </div>
+                      <div className="results">
+                        <h4>{this.state.ipfsHash == "" ? "" : "Your Ipfs CID:"}</h4>
+                        <h5 style={{color: "red"}}>      
+                            {this.state.ipfsHash != "" ? 
+                          "The CID is the identifier of your file in the IPFS. It's very important that you don't lose this information otherwise you might not be able to access your file"
+                          : ""}
+                        </h5>
+                        <h3>{this.state.ipfsHash == "" ? "" : this.state.ipfsHash}</h3>
+                      </div>
+                    </div>
                   </form>
                 </div>
               </main>
             </div>
           </div>
-        </div>
-        <div className='digital signature'>
-          <form onSubmit={this.VerifySignature}>
-            {/* <div>
-              <label>Message</label>
-              <input value={this.state.msg} onChange={(e)=>{this.setState({msg: e.target.value})}} type={"text"}></input>
-            </div> */}
-            <div>
-              <label> upload a file to sign</label>
-              <input type="file" onChange={this.captureFileForSigning}></input>
-            </div>
-            <div>
-              <label>Signature</label>
-              <input value={this.state.signature} onChange={(e)=>{this.setState({signature: e.target.value})}} type={"text"}></input>
-            </div>
-            <div>
-              <label>Public key modulus</label>
-              <input value={this.state.publicKeyModulus} onChange={(e)=>{this.setState({publicKeyModulus: e.target.value})}} type={"text"}></input>
-            </div>
-            <div>
-              <label>Public ket exponent</label>
-              <input value={this.state.publicKeyExponent} onChange={(e)=>{this.setState({publicKeyExponent: e.target.value})}} type={"text"}></input>
-            </div>
-            <input type={"submit"}></input>
-          </form>
         </div>
       </div>
     );
